54d93d664e52878ebc2d36723f905919a77976f9
sucodlaranja <joaoandre210@gmail.com>
2023-04-06 11:52:02 +0100

    added delete and create + better pokemon example

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, Body, Response, APIRouter
+from fastapi import FastAPI, Request, Body, Response, APIRouter, HTTPException
 from fastapi.encoders import jsonable_encoder
 from pymongo import MongoClient
 from models import Pokemon
@@ -12,15 +12,35 @@ collection = db.get_collection("pokemons")
 router = APIRouter()
 
 
-@app.post("/createPokemon", response_model=Pokemon)
-def home(request: Request, pokemon: Pokemon = Body(...)):
+def set_uniqueAttribute():
+    collection.create_index("national_number", unique=True)
+    collection.create_index("paldea_number", unique=True)
+    collection.create_index("name", unique=True)
+
+
+set_uniqueAttribute()
+
+
+@app.post("/createPokemon")
+def createPokemon(request: Request, pokemon: Pokemon = Body(...)):
+    if collection.find_one({"national_number": pokemon.national_number, "paldea_number": pokemon.paldea_number,  "name":  pokemon.name}):
+        raise HTTPException(status_code=400, detail="Pokemon already exists")
     collection.insert_one(pokemon.dict())
     createdPokemon = collection.find_one({"name": pokemon.name})
-    return createdPokemon
+    return "Pokemon created successfully"
 
 
 @app.get("/getPokemons", response_model=list[Pokemon])
-def home(request: Request):
+def get(request: Request):
     pokemons = list(collection.find())
     print(pokemons)
     return pokemons
+
+
+@app.delete("/deletePokemon/{national_number}")
+def deletePokemon(request: Request, national_number: int):
+    deleted = collection.delete_one(
+        {"national_number": national_number}).deleted_count
+    if (deleted == 0):
+        raise HTTPException(status_code=404, detail="Pokemon not found")
+    return {"message": "Pokemon deleted successfully"}
--- models.py
+++ models.py
@@ -1,12 +1,101 @@
 from pydantic import BaseModel, Field
 
 
+class PokemonGender:
+    MALE = "MALE"
+    FEMALE = "FEMALE"
+    UNKNOWN = "UNKNOWN"
+
+
+class PokemonType:
+    NORMAL = "NORMAL"
+    FIRE = "FIRE"
+    WATER = "WATER"
+    ELECTRIC = "ELECTRIC"
+    GRASS = "GRASS"
+    ICE = "ICE"
+    FIGHTING = "FIGHTING"
+    POISON = "POISON"
+    GROUND = "GROUND"
+    FLYING = "FLYING"
+    PSYCHIC = "PSYCHIC"
+    BUG = "BUG"
+    ROCK = "ROCK"
+    GHOST = "GHOST"
+    DRAGON = "DRAGON"
+    DARK = "DARK"
+    STEEL = "STEEL"
+    FAIRY = "FAIRY"
+
+
+class PokemonEgg(BaseModel):
+    egg_steps: int = Field(...)
+    egg_cycles: int = Field(...)
+    egg_group: tuple = Field(...)  # (str, str)
+
+
+class PokemonExperience(BaseModel):
+    base_experience: int = Field(...)
+    experience_growth: str = Field(...)
+
+
+class PokemonAbility:
+    name: str
+    description: str
+    is_hidden: bool
+
+
+class PokemonEffortValue(BaseModel):
+    value: int
+    type: str
+
+
+class PokemonWildHoldItem:
+    name: str
+    rarity: int
+    image_link: str
+
+
 class Pokemon(BaseModel):
-    name: str = Field(...)
-    type: str = Field(...)
+    national_number: int = Field(...)  # id of pokemon
+    paldea_number: int = Field(...)  # id of pokemon in paldea
+    gender: dict = Field(...)  # {gender : probability}
+    name: dict = Field(...)  # {language: name}
+    image_link: dict = Field(...)  # {image_name: link}
+    type: tuple = Field(...)
+    classification: str = Field(...)
+    height: dict = Field(...)  # {unit: height}
+    weight: dict = Field(...)  # {unit: weight}
+    capture_rate: int = Field(...)
+    egg: PokemonEgg = Field(...)
+    # (PokemonAbility, PokemonAbility, PokemonAbility)
+    habilities: tuple = Field(...)
+    experience: PokemonExperience = Field(...)
+    base_happiness: int = Field(...)
+    effort_values: PokemonEffortValue = Field(...)
+    weakness: dict = Field(...)
+    # (PokemonWildHoldItem, PokemonWildHoldItem, PokemonWildHoldItem)
+    wild_hold_item: tuple = Field(...)
 
     class Config:
         schema_extra = {
-            "example": {"name": "Pikachu",
-                        "type": "Electric"}
+            "example": {"national_number": 25,
+                        "paldea_number": 1,
+                        "gender": {"male": 25, "female": 75, "unknown": 0},
+                        "name": {"en": "Pikachu", "es": "Pikachu", "jp": "ピカチュウ"},
+                        "image_link": {"image_name": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"},
+                        "type": ("Electric", "Normal"),
+                        "classification": "Mouse Pokémon",
+                        "height": {"unit": "0.4 m"},
+                        "weight": {"unit": "6.0 kg"},
+                        "capture_rate": 190,
+                        "egg": {"egg_steps": 5120, "egg_cycles": 20, "egg_group": ("Field", "Fairy")},
+                        "habilities": ("Static", "Lightning Rod", "Static"),
+                        "experience": {"base_experience": 112, "experience_growth": "Medium Fast"},
+                        "base_happiness": 70,
+                        "effort_values": {"value": 1, "type": "Speed"},
+                        "weakness": {"Fire": 2, "Water": 2, "Electric": 0.5, "Grass": 0.5, "Ice": 2, "Fighting": 1, "Poison": 1},
+                        "wild_hold_item": ("Potion", "Potion", "Potion")
+
+                        }
         }
