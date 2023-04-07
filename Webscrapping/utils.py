3530ed2609ec544470c5027f119734610e13490f
LCMJ21 <jorge.lima.ne@gmail.com>
2023-04-07 15:33:43 +0100

    imports working on fastAPI

--- Webscrapping/utils.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-
-base_url = "https://www.serebii.net"
-
-def get_html_from_url(url):
-    page = urllib.request.urlopen(url)
-    html = page.read().decode("ISO-8859-1")
-    page.close()
-    return html
