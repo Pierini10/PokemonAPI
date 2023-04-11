ab816ad66fe7b21e9a8507074c9ccc0ce8258308
sucodlaranja <joaoandre210@gmail.com>
2023-04-11 14:48:34 +0100

    know checks all possible stats

--- pokemonapi/utils.py
+++ pokemonapi/utils.py
@@ -61,64 +61,64 @@ def possible_pokemon_searchs(query: SearchPokemon):
         searchParams["stats.Stats.HP"] = int(query.hp)
     if (query.mthp):
         searchParams["$and"].append(
-            {"stats.Stats.HP": {"$gte": int(query.mtHp)}})
+            prepare_stats_comp("HP", query.mthp, ">"))
     if (query.lthp):
         searchParams["$and"].append(
-            {"stats.Stats.HP": {"$lte": int(query.ltHp)}})
+            prepare_stats_comp("HP", query.lthp, "<"))
 
     if (query.attack):
         searchParams["stats.Stats.Attack"] = int(query.attack)
     if (query.mtAttack):
         searchParams["$and"].append(
-            {"stats.Stats.Attack": {"$gte": int(query.mtAttack)}})
+            prepare_stats_comp("Attack", query.mtAttack, ">"))
     if (query.ltAttack):
         searchParams["$and"].append(
-            {"stats.Stats.Attack": {"$lte": int(query.ltAttack)}})
+            prepare_stats_comp("Attack", query.ltAttack, "<"))
 
     if (query.defense):
         searchParams["stats.Stats.Defense"] = int(query.defense)
     if (query.mtDefense):
         searchParams["$and"].append(
-            {"stats.Stats.Defense": {"$gte": int(query.mtDefense)}})
+            prepare_stats_comp("Defense", query.mtdefense, ">"))
     if (query.ltDefense):
         searchParams["$and"].append(
-            {"stats.Stats.Defense": {"$lte": int(query.ltDefense)}})
+            prepare_stats_comp("Defense", query.ltdefense, "<"))
 
     if (query.spAttack):
         searchParams["stats.Stats.Sp. Attack"] = int(query.spAttack)
     if (query.mtSpAttack):
         searchParams["$and"].append(
-            {"stats.Stats.Sp. Attack": {"$gte": int(query.mtSpAttack)}})
+            prepare_stats_comp("Sp. Attack", query.mtSpAttack, ">"))
     if (query.ltSpAttack):
         searchParams["$and"].append(
-            {"stats.Stats.Sp. Attack": {"$lte": int(query.ltSpAttack)}})
+            prepare_stats_comp("Sp. Attack", query.ltSpAttack, "<"))
 
     if (query.spDefense):
         searchParams["stats.Stats.Sp. Defense"] = int(query.spDefense)
     if (query.mtSpDefense):
         searchParams["$and"].append(
-            {"stats.Stats.Sp. Defense": {"$gte": int(query.mtSpDefense)}})
+            prepare_stats_comp("Sp. Defense", query.mtSpDefense, ">"))
     if (query.ltSpDefense):
         searchParams["$and"].append(
-            {"stats.Stats.Sp. Defense": {"$lte": int(query.ltSpDefense)}})
-
+            prepare_stats_comp("Sp. Defense", query.ltSpDefense, "<"))
+        
     if (query.speed):
         searchParams["stats.Stats.Speed"] = int(query.speed)
     if (query.mtSpeed):
         searchParams["$and"].append(
-            {"stats.Stats.Speed": {"$gte": int(query.mtSpeed)}})
+            prepare_stats_comp("Speed", query.mtSpeed, ">"))
     if (query.ltSpeed):
         searchParams["$and"].append(
-            {"stats.Stats.Speed": {"$lte": int(query.ltSpeed)}})
+            prepare_stats_comp("Speed", query.ltSpeed, "<"))
 
     if (query.total):
         searchParams["stats.Stats.Total"] = int(query.total)
     if (query.mtTotal):
         searchParams["$and"].append(
-            {"stats.Stats.Total": {"$gte": int(query.mtTotal)}})
+            prepare_stats_comp("Total", query.mtTotal, ">"))
     if (query.ltTotal):
         searchParams["$and"].append(
-            {"stats.Stats.Total": {"$lte": int(query.ltTotal)}})
+            prepare_stats_comp("Total", query.ltTotal, "<"))
 
     if (query.captureRate):
         searchParams["capture_rate"] = int(query.captureRate)
@@ -198,3 +198,7 @@ def prepareString(string: str):
         item = item[0].upper() + item[1:].lower()
         temp.append(item)
     return " ".join(temp)
+
+
+def prepare_stats_comp(key, value, comp: str):
+    return {'$where': f'function() {{ for (const item of this.stats) {{for (key in item) {{  if (item[key]["{key}"] {comp} {int(value)}) return true; }}}} return false; }}'}
